2cbb0b5bfea93fbc616c6a867bd4a669f6591dfe
Vedant Marodkar <vedantmarodkar11@gmail.com>
2023-11-07 14:00:32 +0530

    added py file

---
+++ ds250.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+dt0 = pd.read_csv('Rape_Data/Rape_under_Different_age_groups_2018.csv')
+dt0.head(11)
